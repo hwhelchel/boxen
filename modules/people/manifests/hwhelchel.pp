6352ddcae93c27b3f48befdb817373a8ae702d13
Harry Whelchel <harrywhelchel@gmail.com>
2016-01-17 11:41:57 -0500

    adds a lot of packages

--- modules/people/manifests/hwhelchel.pp
+++ modules/people/manifests/hwhelchel.pp
@@ -1,3 +1,46 @@
 class people::hwhelchel {
   include sublime_text
+  include onepassword
+  include onepassword::chrome
+
+  include alfred
+  include spotify
+  include sketch
+  include slack
+  include heroku
+  include foreman
+  include osx
+  include erlang
+  include fonts
+  include iterm2
+  include docker
+  include inkscape
+  include libreoffice
+  include julia
+  include chrome
+  include dropbox
+  include wkhtmltopdf
+  include spectacle
+  include evernote
+  include skype
+  include omnifocus
+  include clojure
+  include textual
+  include skitch
+  include elixir
+  include imagemagick
+  include nmap
+  include selfcontrol
+  include gimp
+  include flux
+  include licecap
+  include memcached
+  include openssl
+  include screenhero
+  include zsh
+  include zshgitprompt
+  include handbrake
+  include grandtotal
+  include kindle
+  include wunderlist
 }
